0455d432b0fcfba59ddc28159b19128adaedf32f
MacK Wallace <mackwallace@cmail.carleton.ca>
2020-09-25 09:19:31 -0400

    added a print "Address book" output.

---
+++ src/AddressBook.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+public class AddressBook {
+ArrayList<BuddyInfo> list = new ArrayList<>();
+
+    public static void main(String[] args){
+        System.out.println("Address book");
+    }
+
+    public String addBuddy() {
+        return "yes";
+    }
+    public void removeBuddy() {
+
+    }
+}
