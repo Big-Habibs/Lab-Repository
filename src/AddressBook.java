9141dfe80148efbb37ceeb853639f9263f51c90e
MacK Wallace <mackwallace@cmail.carleton.ca>
2020-10-01 19:21:01 -0400

    added Text

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,19 +1,28 @@
 import java.util.ArrayList;
 public class AddressBook {
-ArrayList<BuddyInfo> list = new ArrayList<>();
-
+    /**
+     * Adding text as a test.
+     */
+    private ArrayList<BuddyInfo> myBuddies;
+    public AddressBook(){
+        myBuddies = new ArrayList<>();
+    }
     public static void main(String[] args){
         BuddyInfo buddy = new BuddyInfo();
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
-        addressBook.removeBuddy(buddy);
+        addressBook.removeBuddy(0);
 
     }
 
     public void addBuddy(BuddyInfo bud) {
-        list.add(bud);
+        myBuddies.add(bud);
     }
-    public void removeBuddy(BuddyInfo bud) {
-        list.remove(bud);
+
+    public BuddyInfo removeBuddy(int index) {
+        if(index >= 0 && index < myBuddies.size()){
+            return myBuddies.remove(index);
+        }
+        return null;
     }
 }
